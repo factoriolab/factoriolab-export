c2c094e203682e5439b388166ee0abb332becce6
Doug Broad <dcbroad3@gmail.com>
2026-01-13 16:42:06 -0500

    Simplify get_row function

--- scripts/get-row.lua
+++ scripts/get-row.lua
@@ -1,19 +1,16 @@
 return function()
-  local last_row, last_col, last_group, last_subgroup = 0, 0
+  local last_row, last_group, last_subgroup = 0
   return function(obj)
     if obj.group == last_group then
       if obj.subgroup ~= last_subgroup then
         last_row = last_row + 1
-        last_col = 0
       end
     else
       last_row = 0
-      last_col = 0
     end
 
     last_group = obj.group
     last_subgroup = obj.subgroup
-    last_col = last_col + 1
     return last_row
   end
 end
